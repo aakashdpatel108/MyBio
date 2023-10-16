dd4cf4ad6b718d681afbc016a3437f8937fe6db3
Aakash <Aakashdpatel108@gmail.com>
2023-10-16 15:56:39 -0400

    change in conn

--- contactme/process_form.php
+++ contactme/process_form.php
@@ -1,7 +1,7 @@
 <?php
 // PHP Data Objects(PDO) Sample Code:
 try {
-    $conn = new PDO("sqlsrv:server = tcp:dbforbio.database.windows.net,1433; Database = biodb", "localhost", "{Aakash@123}");
+    $conn = new PDO("sqlsrv:server = tcp:dbforbio.database.windows.net,1433; Database = biodb", "localhost", "Aakash@123");
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 }
 catch (PDOException $e) {
@@ -10,7 +10,7 @@ catch (PDOException $e) {
 }
 
 // SQL Server Extension Sample Code:
-$connectionInfo = array("UID" => "localhost", "pwd" => "{Aakash@123}", "Database" => "biodb", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
+$connectionInfo = array("UID" => "localhost", "pwd" => "Aakash@123", "Database" => "biodb", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
 $serverName = "tcp:dbforbio.database.windows.net,1433";
 $conn = sqlsrv_connect($serverName, $connectionInfo);
 
