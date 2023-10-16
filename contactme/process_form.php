7b6d12fa579946c4221a1ad317e811d450bb3dd2
aakashdpatel108 <Aakashdpatel108@gmail.com>
2023-10-16 12:46:51 -0400

    Initial commit

---
+++ contactme/process_form.php
@@ -0,0 +1,30 @@
+<?php
+$servername = "localhost";  // Replace with your MySQL server name (usually "localhost" if it's on the same machine)
+$username = "root";         // MySQL username
+$password = ""; // MySQL password (the one you set during installation)
+$database = "biodb";    // Your MySQL database name
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $database);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+// Get form data
+$name = $_POST['name'];
+$email = $_POST['email'];
+$message = $_POST['message'];
+
+// Insert data into the database
+$sql = "INSERT INTO entry (name, email, message) VALUES ('$name', '$email', '$message')";
+
+if ($conn->query($sql) === TRUE) {
+    echo "New record created successfully";
+} else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+$conn->close();
+?>
---
+++ contactme/script.js
@@ -0,0 +1,8 @@
+document.getElementById("contact-form").addEventListener("submit", function(event) {
+    event.preventDefault();
+
+    // You can add code here to handle form submission, for example, sending form data to a server using AJAX.
+
+    // For the sake of this example, let's just show an alert.
+    alert("Form submitted!");
+});
