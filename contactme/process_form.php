7f77a418030464c9b7a6fc79636d66394d8ae990
Aakash <aakashdpatel108@gmail.com>
2023-10-17 20:33:39 +0000

    code change in conn from vm

--- contactme/process_form.php
+++ contactme/process_form.php
@@ -1,31 +1,29 @@
 <?php
-// PHP Data Objects(PDO) Sample Code:
+
 try {
+    // Using PDO (PHP Data Objects)
     $conn = new PDO("sqlsrv:server = tcp:dbforbio.database.windows.net,1433; Database = biodb", "localhost", "Aakash@123");
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-}
-catch (PDOException $e) {
+
+    // Using SQL Server Extension (sqlsrv)
+    // $serverName = "tcp:dbforbio.database.windows.net,1433";
+    // $connectionOptions = array(
+    //     "Database" => "biodb",
+    //     "Uid" => "your_username_here",
+    //     "PWD" => "your_password_here"
+    // );
+    // $conn = sqlsrv_connect($serverName, $connectionOptions);
+
+    if ($conn) {
+        echo "Connected to Azure SQL Database successfully!";
+        // You can perform database operations here
+    } else {
+        echo "Error connecting to Azure SQL Database.";
+    }
+} catch (PDOException $e) {
     print("Error connecting to SQL Server.");
     die(print_r($e));
 }
-
-// SQL Server Extension Sample Code:
-// $connectionInfo = array("UID" => "localhost", "pwd" => "Aakash@123", "Database" => "biodb", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
-// $serverName = "tcp:dbforbio.database.windows.net,1433";
-// $conn = sqlsrv_connect($serverName, $connectionInfo);
-
-// $servername = "localhost";  // Replace with your MySQL server name (usually "localhost" if it's on the same machine)
-// $username = "root";         // MySQL username
-// $password = ""; // MySQL password (the one you set during installation)
-// $database = "biodb";    // Your MySQL database name
-
-// // Create connection
-// $conn = new mysqli($servername, $username, $password, $database);
-
-// // Check connection
-// if ($conn->connect_error) {
-//     die("Connection failed: " . $conn->connect_error);
-// }
 
 // Get form data
 $name = $_POST['name'];
